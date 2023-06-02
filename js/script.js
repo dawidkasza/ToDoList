a9f7ec654f8c7802afb4ccdaa10267f13d09ecb4
Dawid Kasza <dawidkasza@op.pl>
2023-06-02 12:50:40 +0200

    Add render function

--- js/script.js
+++ js/script.js
@@ -1,6 +1,34 @@
-{
-    const init = () => {
+{   
+    const tasks = [
+        {
+            content: "test1",
+            done: false,
+        },
+        {
+            content: "test2",
+            done: true,
+        },
+    ];
 
+    const render = () => {
+        let tasksListHTMLContent = "";
+        for(const task of tasks){
+            tasksListHTMLContent += `
+                <li class="task__item">
+                <button class=" task__button task__button--toggleDone"> ${task.done ? "✔" : ""}</button>
+                <span class="task__content ${task.done ? " task__content--done" : ""}"> 
+                    ${task.content}
+                </span>
+                <button class=" task__button task__button--remove">X</button>
+                </li>
+            `
+        }
+
+        document.querySelector(".js-tasks").innerHTML = tasksListHTMLContent;
+    };
+
+    const init = () => {
+        render();
     };
 
     init();
